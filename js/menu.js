9972c2c5909d7ac2384ac284e4887a65e677c65a
Aleksey Gelich <alexeygelich@gmail.com>
2020-09-13 13:20:44 +0300

    запрещает скролл body при открытом мобильном меню через скрипт

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
 (() => {
+  const bodyStopScroll = document.querySelector('[data-body]');
   const menuBtnRef = document.querySelector('[data-menu-button]');
   menuBtnRef.addEventListener('click', () => {
     menuBtnRef.classList.toggle('is-open');
+    bodyStopScroll.classList.toggle('stop-scroll');
   });
 })();
